4e24c6374a62a8c318df561b3ec1890326c89942
huoyitong <349247397@qq.com>
2020-01-16 23:23:13 +0800

    dev: dev

---
+++ ts/src/generice.ts
@@ -0,0 +1,20 @@
+// 泛型
+function log<T>(value: T): T {
+    console.log(value);
+    return value;
+}
+
+// 两种调用方式 推荐第二种
+log<string[]>(['a', 'b'])
+log(['a', 'b'])
+
+// type Log = <T>(value: T) => T
+// let myLog: Log = log
+
+// 泛型接口
+interface Log<T = string> {
+    (value: T): T
+}
+
+let myLog: Log = log
+myLog('1')
