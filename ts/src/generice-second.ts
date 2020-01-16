8b57316b12c0004a0afa4b9a4ae7b09e494a19e6
huoyitong <349247397@qq.com>
2020-01-16 23:29:17 +0800

    dev: dev

---
+++ ts/src/generice-second.ts
@@ -0,0 +1,30 @@
+class Log<T> {
+    run(value: T) {
+        console.log(value)
+        return value
+    }
+}
+
+let log1 = new Log<number>()
+log1.run(1)
+let log2 = new Log()
+log2.run('a')
+
+// 泛型约束
+interface Length {
+    length: number
+}
+
+function log<T extends Length>(value: T): T {
+    console.log(value, value.length)
+    return value
+}
+
+log([1])
+log('123')
+log({length: 1})
+
+// 1函数和类可以轻松地支持多种类型，增强程序的扩展性
+// 2不必写多条函数重载，冗长的联合类型声明，增强代码可读性
+// 3灵活控制类型之间的约束
+
