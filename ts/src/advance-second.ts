fcd9d0a27b37fff3c4938746efa1169ee840628f
huoyitong <349247397@qq.com>
2020-01-18 00:21:24 +0800

    dev: dev

--- ts/src/advance-second.ts
+++ ts/src/advance-second.ts
@@ -33,3 +33,32 @@ function getPet(master: Master) {
     return pet
 }
 
+interface Square {
+    kind: "square";
+    size: number;
+}
+interface Rectangle {
+    kind: "rectangle";
+    width: number;
+    height: number;
+}
+
+interface Circle {
+    kind: 'circle';
+    r: number
+}
+type Shape = Square | Rectangle | Circle
+
+function area(s: Shape){
+    switch (s.kind) {
+        case "square":
+            return s.size * s.size;
+        case "rectangle":
+            return s.height * s.width;
+        case "circle":
+            return Math.PI * s.r * 2;
+        default: 
+            return ((e: never) => {throw new Error(e)})(s)
+    }
+}
+console.log(area({kind: 'circle', r: 1}))
