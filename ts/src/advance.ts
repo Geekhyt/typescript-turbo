f022a4a4e35db4e17bb0374671c7c270d201a4a9
huoyitong <349247397@qq.com>
2020-01-18 00:05:26 +0800

    dev: dev

--- ts/src/advance.ts
+++ ts/src/advance.ts
@@ -154,23 +154,60 @@ log11 =log22
 // 函数之间兼容：参数多的兼容参数少的
 
 // 类型保护
+// TypeScript能够在特定的区块中保证变量属于某种特定的类型
+// 可以在此区块中放心地引用此类型的属性，或者调用此类型的方法
 enum Type { Strong, Week }
 
 class Java {
     helloJava() {
         console.log('Hello Java')
     }
+    java: any
 }
 
 class JavaScript {
     helloJavaScript() {
         console.log('Hello JavaScript')
     }
+    javascript: any
 }
 
-function getLanguage(type: Type) {
+function isJava(lang: Java | JavaScript): lang is Java {
+    return (lang as Java).helloJava !== undefined
+}
+
+function getLanguage(type: Type, x: string | number) {
     let lang = type === Type.Strong ? new Java() : new JavaScript()
+    // if ((lang as Java).helloJava) {
+    //     (lang as Java).helloJava()
+    // } else {
+    //     (lang as JavaScript).helloJavaScript()
+    // }
+    // 1.instanceof
+    // if (lang instanceof Java) {
+    //     lang.helloJava()
+    // } else {
+    //     lang.helloJavaScript()
+    // }
+    // 2.in
+    // if ('java' in lang) {
+    //     lang.helloJava()
+    // } else {
+    //     lang.helloJavaScript()
+    // }
+    // 3.typeof
+    // if (typeof x === 'string') {
+    //     x.length
+    // } else {
+    //     x.toFixed(2)
+    // }
+    // 4.自定义函数
+    if (isJava(lang)) {
+        lang.helloJava()
+    } else {
+        lang.helloJavaScript()
+    }
     return lang
 }
 
-getLanguage(Type.Strong)
+// getLanguage(Type.Strong)
