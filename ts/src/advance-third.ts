449f482d8b2bd129a133eefe9d66c0f22de3321e
huoyitong <349247397@qq.com>
2020-01-18 00:30:53 +0800

    dev: dev

--- ts/src/advance-third.ts
+++ ts/src/advance-third.ts
@@ -23,3 +23,21 @@ let value: Obj['a']
 
 // T extends U
 // 如上改造
+
+
+// 映射类型
+interface Obj1 {
+    a: string;
+    b: number;
+    c: boolean;
+}
+
+// 同态类型
+type ReadonlyObj = Readonly<Obj1>
+
+type PartialObj = Partial<Obj1>
+
+type PickObj = Pick<Obj1, 'a' | 'b'>
+
+// 非同态类型
+type RecordObj = Record<'x' | 'y', Obj1>
