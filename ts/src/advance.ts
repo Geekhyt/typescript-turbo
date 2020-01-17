cddc3052a2127806cd8504d8f2f338a7caa6b16f
huoyitong <349247397@qq.com>
2020-01-17 23:52:54 +0800

    dev: dev

--- ts/src/advance.ts
+++ ts/src/advance.ts
@@ -26,3 +26,129 @@ let foo: Foo = {
 // 类型断言
 // 类型保护
 
+// 类型兼容
+let s: string = 'a'
+s = null
+
+// 接口兼容性
+interface X {
+    a: any;
+    b: any;
+}
+
+interface Y {
+    a: any;
+    b: any;
+    c: any;
+}
+
+let xx: X = { a: 1, b: 2 }
+let yy: Y = { a: 1, b: 2, c: 3 } 
+xx = yy
+// yy = xx
+
+// 函数兼容性
+type Handler = (a: number, b: number) => void
+function hof(handler: Handler) {
+    return handler
+}
+// 函数参数
+let handler1 = (a: number) => {}
+hof(handler1)
+
+let handler2 = (a: number, b: number, c: number) => {}
+// hof(handler2)
+
+// 可选参数和剩余参数
+let aaa = (p1: number, p2: number) => {}
+let bbb = (p1?: number, p2?: number) => {}
+let ccc = (...args: number[]) => {}
+aaa = bbb
+aaa = ccc
+bbb = aaa
+bbb = ccc
+ccc = aaa
+ccc = bbb
+
+// 参数类型
+let handler3 = (a: string) => {}
+// hof(handler3)
+
+interface Point3D {
+    x: number;
+    y: number;
+    z: number;
+}
+
+interface Point2D {
+    x: number;
+    y: number;
+}
+
+let p3d = (point: Point3D) => {}
+let p2d = (point: Point2D) => {}
+p3d = p2d
+// p2d = p3d
+
+// 返回值类型
+let ff = () => ({ name: 'Alice' })
+let gg = () => ({ name: 'Alice', location: 'Beijing' })
+ff = gg
+// gg = ff
+
+function overload(a: number, b: number): number;
+function overload(a: string, b: string): string;
+function overload(a: any, b: any, c: any): any{}
+
+// 枚举兼容性
+enum Fruit { Apple, Banana }
+enum Color { Red, Yellow }
+let fruit: Fruit.Apple = 3
+let no: number = Fruit.Apple
+// let color: Color.Red = Fruit.Apple
+
+// 类兼容性
+class A {
+    constructor(p: number, q: number) {}
+    id: number = 1
+    private name: string = ''
+}
+class B {
+    static s = 1
+    constructor(p: number) {}
+    id: number = 2
+    private name: string = ''
+}
+let aaaa = new A(1, 2)
+let bbbb = new B(1)
+// aaaa = bbbb
+// bbbb = aaaa
+class CC extends A {}
+let cccc = new CC(1, 2)
+aaaa = cccc
+cccc = aaaa
+
+// 泛型兼容性
+interface Empty<T> {
+    value: T
+}
+// let obj1: Empty<number> = {};
+// let obj2: Empty<string> = {};
+// obj1 = obj2
+
+// 泛型函数
+let log11 = <T>(x: T): T => {
+    console.log('x')
+    return x
+}
+let log22 = <U>(y: U): U => {
+    console.log('y')
+    return y
+}
+log11 =log22
+
+// 当一个类型Y可以被赋值给另一个类型X时，我们就可以说类型X兼容类型Y
+// X 兼容 Y: X(目标类型) = Y(源类型)
+// 口诀：
+// 结构之间兼容：成员少的兼容成员多的
+// 函数之间兼容：参数多的兼容参数少的
