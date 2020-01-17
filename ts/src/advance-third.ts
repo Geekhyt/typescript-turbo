e4e740242d423044bbd302bd3639ce7cfa0e384f
huoyitong <349247397@qq.com>
2020-01-18 00:42:52 +0800

    dev: dev

--- ts/src/advance-third.ts
+++ ts/src/advance-third.ts
@@ -41,3 +41,38 @@ type PickObj = Pick<Obj1, 'a' | 'b'>
 
 // 非同态类型
 type RecordObj = Record<'x' | 'y', Obj1>
+
+// 条件类型
+// T extend U ? X : Y
+type TypeName<T> = 
+    T extends string ? "string" :
+    T extends number ? "number" :
+    T extends boolean ? "boolean" :
+    T extends undefined ? "undefined" :
+    T extends Function ? "function" :
+    "object";
+type T1 = TypeName<string>
+type T2 = TypeName<string[]>
+
+// (A | B) extend U ? X : Y
+// (A extends U ? X : Y) | (B extends U ? X : Y)
+type T3 = TypeName<string | string[]>
+
+type Diff<T, U> = T extends U ? never : T
+
+type T4 = Diff<"a" | "b" | "c", "a" | "e">
+// Diff<"a", "a" | "e"> | Diff<"b", "a" | "e"> | Diff<"c", "a" | "e">
+// never | "b" | "c"
+// "b" | "c"
+
+type NotNull<T> = Diff<T, undefined | null>
+type T5 = NotNull<string | number | undefined | null>
+
+// Exclude<T, U> 如上Diff
+// NonNullable<T> 如上NotNull
+
+// Extratct<T, U>
+type T6 = Extract<"a" | "b" | "c", "a" | "e">
+
+// RetrunType<T>
+type T7 = ReturnType<() => string>
